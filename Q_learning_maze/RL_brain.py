18c6a7e85cbf473eae35483bb9dba31134f52ce4
djbitbyte <janetjied@gmail.com>
2018-01-28 21:32:35 +0100

    Q learning for maze & .gitignore added.

---
+++ Q_learning_maze/RL_brain.py
@@ -0,0 +1,44 @@
+import numpy as np
+import pandas as pd
+
+class QLearningTable:
+	def __init__(self, actions, learning_rate=0.01, reward_decay=0.9, e_greedy=0.9):
+		self.actions = actions  
+		self.lr = learning_rate
+		self.gamma = reward_decay
+		self.epsilon = e_greedy
+		self.q_table = pd.DataFrame(columns=self.actions, dtype=np.float)
+
+	def choose_action(self, observation):
+		self.check_state_exist(observation)
+		# action selection
+		if np.random.uniform() < self.epsilon:
+			state_action = self.q_table.ix[observation, :]
+			# shaffle actions in the state
+			state_action = state_action.reindex(np.random.permutation(state_action.index))
+			action = state_action.idxmax()
+		else:
+			# choose random action
+			action = np.random.choice(self.actions)
+		return action
+
+	def learn(self, s, a, r, s_):
+		self.check_state_exist(s_)
+		q_predict = self.q_table.ix[s, a]
+		if s_ != 'terminal':
+			q_target = r + self.gamma * self.q_table.ix[s_, :].max()
+		else:
+			q_target = r
+		self.q_table.ix[s, a] += self.lr * (q_target - q_predict)   # update
+		print(self.q_table)
+
+	def check_state_exist(self, state):
+		if state not in self.q_table.index:
+			# append new state to q table
+			self.q_table = self.q_table.append(
+				pd.Series(
+					[0]*len(self.actions),
+					index=self.q_table.columns,
+					name=state))
+
+
